a1c2f5e9fe4cf6189a71799072dd8f42ae95eac5
mohieey <Mohhiey@gmail.com>
2023-12-31 13:14:55 +0200

    modify broker service

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -10,11 +10,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (cfg *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -39,6 +45,8 @@ func (cfg *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		cfg.authenticate(w, requestPayload.Auth)
+	case "log":
+		cfg.logItem(w, requestPayload.Log)
 	default:
 		cfg.errorJSON(w, errors.New("unknown action"))
 	}
@@ -86,6 +94,35 @@ func (cfg *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	payload.Error = false
 	payload.Message = "Authenticated!"
 	payload.Data = jsonFromService.Data
+
+	cfg.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (cfg *Config) logItem(w http.ResponseWriter, log LogPayload) {
+	jsonData, _ := json.MarshalIndent(log, "", "\t")
+
+	request, err := http.NewRequest(http.MethodPost, "http://logger-service/log", bytes.NewBuffer(jsonData))
+	if err != nil {
+		cfg.errorJSON(w, err)
+		return
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		cfg.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		cfg.errorJSON(w, errors.New("error calling log service"))
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "logged successfully"
 
 	cfg.writeJSON(w, http.StatusAccepted, payload)
 }
